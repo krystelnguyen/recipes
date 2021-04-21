a5a770afd029cc7c3e06982533e0bdf4ba31ac3f
krystelnguyen <56112019+krystelnguyen@users.noreply.github.com>
2021-04-21 14:08:56 +0200

    Search input

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
-import { Search } from './Search';
-
 export function Recipes() {
     const [recipes, setRecipes] = React.useState([]);
     const [search, setSearch] = React.useState('');
